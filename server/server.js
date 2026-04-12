ff5cbaab4efa7ad78023ddfa60d3387f07f0a491
Debarup <87480898+debarup24@users.noreply.github.com>
2026-04-13 00:38:10 +0530

    code refactor

--- server/server.js
+++ server/server.js
@@ -1,27 +1,48 @@
-import express from "express" ;
-import cors from "cors" ;
-import "dotenv/config" ;
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import emailRouter from "./routes/emailRoutes.js";
 
-
-const app = express() ;
+const app = express();
 connectDB();
-const port = process.env.PORT || 4000
 
-const allowedOrigins = [ "http://localhost:5174" , "https://auth-mern-client-delta.vercel.app"]
+// connectDB() Can be written in production level : We should wait for DB before starting server -
+//   const startServer = async () => {
+//     try {
+//       await connectDB();
 
-app.use(express.json()) ;
-app.use (cookieParser()) ;
-app.use (cors({origin: allowedOrigins , credentials: true})) ;
+//       app.listen(PORT, () => {
+//         console.log(`Server running on port ${PORT}`);
+//       });
+//     } catch (error) {
+//       console.error("Failed to start server:", error);
+//       process.exit(1);
+//     }
+//   };
+
+//   startServer();
+
+const port = process.env.PORT || 4000;
+
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://auth-mern-client-delta.vercel.app",
+];
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true })); // here credentials: true mean it tells the browser: It’s allowed to send credentials like cookies, JWT Token.. with cross-origin requests. By default, browsers block credentials in cross-origin requests
 
 // API Endpoints
-app.get('/', (req, res) => res.send("Hello Ji.. API Working fine")) ;
-app.use("/api/auth", authRouter) ;
-app.use("/api/user", userRouter) ;
-app.use("/api/email", emailRouter) ;
+app.get("/", (req, res) => res.send("Hello Ji.. API Working fine"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/email", emailRouter);
 
-app.listen(port, () => console.log(`Server sucessfully started on PORT : ${port}`)) ;
+app.listen(port, () =>
+  console.log(`Server sucessfully started on PORT : ${port}`),
+);
